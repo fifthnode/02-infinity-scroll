459c08d000da94ae496ca8a3d681c504827658b9
Randall Cox <rcoxwork@gmail.com>
2020-10-27 16:01:22 -0700

    JavaScript infinite scroll, first pass

--- script.js
+++ script.js
@@ -21,17 +21,12 @@ function displayPhotos() {
   photosArray.forEach(photo => {
     // Create <a> to link to Unsplash
     const item = document.createElement('a');
-    // item.setAttribute('href', photo.links.html);
-    // item.setAttribute('target', '_blank');
     setAttributes(item, {
       href: photo.links.html,
       target: '_blank',
     });
     // Create <img> for photo
     const img = document.createElement('img');
-    // img.setAttribute('src', photo.urls.regular);
-    // img.setAttribute('alt', photo.alt_description);
-    // img.setAttribute('title', photo.alt_description);
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
@@ -47,15 +42,25 @@ function displayPhotos() {
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    // const data = await response.json();
-    // console.log(data);
     photosArray = await response.json();
-    // console.log(photosArray);
     displayPhotos();
   } catch (error) {
     // Catch error here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+  // console.log('scrolled');
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    getPhotos();
+    // console.log('window.innerHeight', window.innerHeight);
+    // console.log('window.scrollY', window.scrollY);
+    // console.log('window.innerHeight + window.scrollY', window.innerHeight + window.scrollY);
+    // console.log('document.body.offsetHeight - 1000', document.body.offsetHeight - 1000);
+    console.log('load more');
+  }
+});
 
 // On load
 getPhotos();
